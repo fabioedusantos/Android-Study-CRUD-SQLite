7e1e62e2183e3d49df1daef7da9babddc98661f8
Fábio Eduardo dos Santos <fabioedusantos@gmail.com>
2018-04-20 22:34:00 -0300

    feat(users): integra listagem de usuários à interface

--- app/src/main/java/com/fabio/professor/crudcommanipulaodeobjetosgrficos/MainActivity.java
+++ app/src/main/java/com/fabio/professor/crudcommanipulaodeobjetosgrficos/MainActivity.java
@@ -3,9 +3,17 @@ package com.fabio.professor.crudcommanipulaodeobjetosgrficos;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.fabio.professor.crudcommanipulaodeobjetosgrficos.dao.DaoAdapter;
+import com.fabio.professor.crudcommanipulaodeobjetosgrficos.dao.DaoUsuarios;
+import com.fabio.professor.crudcommanipulaodeobjetosgrficos.domain.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText txtNome;
     private EditText txtSenha;
     private EditText txtConfirme;
+    private Usuario usuario;
+    private DaoUsuarios daoUsuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +35,41 @@ public class MainActivity extends AppCompatActivity {
         txtNome = findViewById(R.id.txtNome);
         txtSenha = findViewById(R.id.txtSenha);
         txtConfirme = findViewById(R.id.txtConfirme);
+
+        usuario = new Usuario();
+
+        DaoAdapter daoAdapter = new DaoAdapter(this);
+        daoAdapter.onCreate(daoAdapter.getWritableDatabase());
+        daoUsuarios = new DaoUsuarios(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        List<String> lista = new ArrayList<>();
+        final List<Usuario> usuarios = daoUsuarios.get();
+
+        if (usuarios.size() > 0) setTitle("Usuários Cadastrados");
+        else setTitle("Não há usuários cadastrados");
+
+        for (Usuario u : usuarios) lista.add(u.getNome());
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1,
+                lista);
+        listView.setAdapter(adapter);
     }
 
     public void salvar(View v) {
     }
 
     public void limpar(View v) {
+        limpar();
+    }
+
+    private void limpar() {
+        usuario = new Usuario();
         txtNome.setText("");
         txtSenha.setText("");
         txtConfirme.setText("");
