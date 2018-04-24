cf7c1942a0c61fee503d434fe270adccbab67bce
Fábio Eduardo dos Santos <fabioedusantos@gmail.com>
2018-04-23 21:09:00 -0300

    feat(users): adiciona cadastro com validação de campos

--- app/src/main/java/com/fabio/professor/crudcommanipulaodeobjetosgrficos/MainActivity.java
+++ app/src/main/java/com/fabio/professor/crudcommanipulaodeobjetosgrficos/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fabio.professor.crudcommanipulaodeobjetosgrficos.dao.DaoAdapter;
 import com.fabio.professor.crudcommanipulaodeobjetosgrficos.dao.DaoUsuarios;
@@ -16,7 +17,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     private ListView listView;
     private TextView lblTitulo;
     private EditText txtNome;
@@ -29,15 +29,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         listView = findViewById(R.id.listView);
         lblTitulo = findViewById(R.id.lblTitulo);
         txtNome = findViewById(R.id.txtNome);
         txtSenha = findViewById(R.id.txtSenha);
         txtConfirme = findViewById(R.id.txtConfirme);
-
         usuario = new Usuario();
-
         DaoAdapter daoAdapter = new DaoAdapter(this);
         daoAdapter.onCreate(daoAdapter.getWritableDatabase());
         daoUsuarios = new DaoUsuarios(this);
@@ -46,28 +43,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-
         List<String> lista = new ArrayList<>();
         final List<Usuario> usuarios = daoUsuarios.get();
-
         if (usuarios.size() > 0) setTitle("Usuários Cadastrados");
         else setTitle("Não há usuários cadastrados");
-
         for (Usuario u : usuarios) lista.add(u.getNome());
-
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1,
-                lista);
+                android.R.layout.simple_list_item_1, android.R.id.text1, lista);
         listView.setAdapter(adapter);
     }
 
     public void salvar(View v) {
+        if (txtNome.getText().toString().length() < 3) {
+            txtNome.setError("Nome deve ter ao menos 3 caracteres.");
+            return;
+        }
+        if (txtSenha.getText().toString().length() < 6) {
+            txtSenha.setError("Senha deve ter ao menos 6 caracteres.");
+            return;
+        }
+        if (!txtSenha.getText().toString().equals(txtConfirme.getText().toString())) {
+            txtConfirme.setError("As senhas devem ser iguais.");
+            return;
+        }
+        usuario.setNome(txtNome.getText().toString());
+        usuario.setSenha(txtSenha.getText().toString());
+
+        if (daoUsuarios.insert(usuario)) {
+            Toast.makeText(this, "Sucesso!", Toast.LENGTH_SHORT).show();
+            limpar();
+            onResume();
+        } else {
+            Toast.makeText(this, "Erro!", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public void limpar(View v) {
-        limpar();
-    }
-
+    public void limpar(View v) { limpar(); }
     private void limpar() {
         usuario = new Usuario();
         txtNome.setText("");
